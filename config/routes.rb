e5a12ae15ac3989598a8de65c237a3d9f96c3cf8
Kenji Fujiwara <niyaton@gmail.com>
2017-09-19 15:17:29 +0900

    maybe no meaningful change

--- config/routes.rb
+++ config/routes.rb
@@ -34,7 +34,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # ログイン画面をホームにする
-  root :to => "home#index"
+  root :to => 'home#index'
 
 
 
